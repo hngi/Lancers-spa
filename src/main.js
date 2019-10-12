8dbe44bcc84dab86de1e3416a2c21a34faca00e9
Osueke Christian <osueke.christian@gmail.com>
2019-10-12 13:28:08 +0100

    Setup store, axios and router scafolds

--- src/main.js
+++ src/main.js
@@ -5,6 +5,30 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+router.beforeEach(
+  (to, from, next)=>{
+
+    if(!to.matched.some(record => record.meta.forAuth)){ //Confirm User is a visitor, otherwise take him to an authenticated route
+      if( (localStorage.getItem('lancers_token')) && (localStorage.getItem('lancers_expiration')) && (Date.now() < parseInt(localStorage.getItem('lancers_expiration'))) ) {
+        next({ path: '/'});
+      }
+      else{
+        next();
+      }
+    }else if(to.matched.some(record => record.meta.forAuth)){ // User has to be authenticated else bounce user to login page
+        if( (localStorage.getItem('lancers_token')) && (localStorage.getItem('lancers_expiration')) && (Date.now() < parseInt(localStorage.getItem('lancers_expiration'))) ) {
+          next();
+        }
+        else{
+          next({ path: '/login'});
+        }
+    }else {
+      next();
+    }
+    
+  }
+);
+
 new Vue({
   router,
   store,
